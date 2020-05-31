7b6ea9034e033a65e203d68b1ebd2dff10a99214
Max Alva <malva@peruapps.com.pe>
2020-05-31 13:35:54 -0500

    listado de tareas

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { db } from '../firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        todos: []
+    },
+    mutations: {
+        setTodos (state, payload) {
+            state.todos = payload
+        }
+    },
+    actions: {
+        getTodos ({ commit }) {
+            const todos = []
+            db.collection('todos').get()
+            .then(collection => {
+                collection.forEach(doc => {
+                    const todo = {
+                        ...doc.data(),
+                        id: doc.id
+                    }
+                    todos.push(todo)
+                })
+                commit('setTodos', todos)
+            })
+        }
+    },
+    modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="home">
-        Home
+    <div>
+        <h1>Inicio</h1>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                {{todo.name}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 
+    import { mapActions, mapState } from 'vuex'
+
     export default {
-        name: 'Home'
+        name: 'Home',
+
+        created () {
+            this.getTodos()
+        },
+        methods: {
+            ...mapActions(['getTodos'])
+        },
+        computed: {
+            ...mapState(['todos'])
+        }
     }
 </script>
